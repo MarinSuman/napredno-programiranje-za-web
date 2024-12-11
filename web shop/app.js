6ab00e805031e4f0dcc0c0fdb01fa42ba19a9e82
Marin Suman <marinsuman69@gmail.com>
2024-12-11 11:29:13 +0100

    cp

--- web shop/app.js
+++ web shop/app.js
@@ -1,11 +1,22 @@
 const express = require('express')
-
-const credentials = {
-    userName: 'bob@gmail.com',
-    password: '1234'
-}
+const mysql = require('mysql2')
 
 const app = express()
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'pasword',
+    database: 'webshop'
+})
+
+db.connect((error)=> {
+    if (error) {
+        console.log('Error connecting to db',error)
+        return
+    }
+    console.log('Db connection succesfull')
+})
 
 app.set('view engine', 'ejs')
 
@@ -15,5 +26,11 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
+    db.query('select * from products', (error, resoult)=> {
+        if (error) {
+            console.log('error selecting products', error)
+        }
+        console.log('products are:', resoult)
+    })
     res.render('index', { title: 'Products' })
 })
